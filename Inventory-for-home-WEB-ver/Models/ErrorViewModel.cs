480c184485fde48517c012a4ba5a9a78c720a82f
Yazbeth <yazryrg@outlook.com>
2024-08-10 20:14:01 -0600

    +se creo el proyecto

---
+++ Inventory-for-home-WEB-ver/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Inventory_for_home_WEB_ver_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
