d6e8170a6c9db87b502ce1707f67ad21c05487cb
Yazbeth <yazryrg@outlook.com>
2024-08-15 22:32:39 -0600

    +Actualizacion proyecto

---
+++ Inventori-for-home-WEB-ver/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Inventori_for_home_WEB_ver_.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Inventori_for_home_WEB_ver_.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        //crear las vistas principales
+        //Inventario
+        public IActionResult Inventario()
+        {
+            return View();
+        }
+
+        //Reglas de prioridad
+        public IActionResult Reglasdeprioridad()
+        {
+            return View();
+        }
+
+        //Tipos de empaques
+        public IActionResult Empaques()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Inventori-for-home-WEB-ver/wwwroot/js/EmpaquesFuncJS.js
@@ -0,0 +1,23 @@
+﻿function llenarTabla() {
+    //SE VAN A TRAER DE BD LOS DATOS
+    //SE VAN A PINTAR EN LA TABLA
+    var grid = document.getElementById("gridEmpaques").getElementsByTagName('tbody')[0]
+    console.log("DETECTANDO");
+    console.log(document.getElementById("gridEmpaques").getElementsByTagName('tbody')[0]);
+
+    var nuevafila = grid.insertRow();
+
+    var celdaId = nuevafila.insertCell(0);
+    var celdaEmpaqueNombre = nuevafila.insertCell(1);
+
+    celdaId.innerHTML = "2";
+    celdaEmpaqueNombre.innerHTML = "PRUEBA EMPAQUE";
+}
+
+function showAlerta() {
+    Swal.fire({
+        title: "Good job!",
+        text: "You clicked the button!",
+        icon: "success"
+    });
+}
